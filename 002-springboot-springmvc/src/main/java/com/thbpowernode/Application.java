a25d762a0746cdf081b5ed06364ef0f0fd34ab82
涂瀚滨 <malakathb@163.com>
2021-08-11 22:09:13 +0800

    002 commit03

--- 002-springboot-springmvc/src/main/java/com/thbpowernode/Application.java
+++ 002-springboot-springmvc/src/main/java/com/thbpowernode/Application.java
@@ -3,8 +3,10 @@ package com.thbpowernode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication//开启springboot配置
 public class Application {
+
+    //springboot项目的代码必须放在Application同级目录或者下级目录里面
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
