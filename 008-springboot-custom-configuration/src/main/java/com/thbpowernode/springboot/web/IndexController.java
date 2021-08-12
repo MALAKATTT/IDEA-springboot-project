82d13f6107ba85f6a9f392e0746658a68e694a44
涂瀚滨 <malakathb@163.com>
2021-08-12 23:09:27 +0800

    008 commit

---
+++ 008-springboot-custom-configuration/src/main/java/com/thbpowernode/springboot/web/IndexController.java
@@ -0,0 +1,21 @@
+package com.thbpowernode.springboot.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+    @Value("${school.name}")
+    private String schoolname;
+
+    @Value("${websit}")
+    private String websit;
+
+    @RequestMapping(value = "/say")
+    public @ResponseBody String say(){
+        return "Hello" + schoolname + ":" + websit;
+    }
+}
